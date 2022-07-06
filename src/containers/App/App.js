5e9ef855582dca8d1cdd3ee8f0d07e2659301abc
Webmaster <webmaster19941022@gmail.com>
2022-07-06 18:08:36 +0200

    initial project

---
+++ src/containers/App/App.js
@@ -0,0 +1,60 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import DocumentMeta from 'react-document-meta';
+import { isLoaded as isInfoLoaded, load as loadInfo } from 'redux/modules/info';
+import { isLoaded as isAuthLoaded, load as loadAuth, logout } from 'redux/modules/auth';
+import { pushState } from 'redux-router';
+import config from '../../config';
+
+@connect(
+  state => ({user: state.auth.user}),
+  {logout, pushState})
+export default class App extends Component {
+  static propTypes = {
+    children: PropTypes.object.isRequired,
+    user: PropTypes.object,
+    logout: PropTypes.func.isRequired,
+    pushState: PropTypes.func.isRequired
+  };
+
+  static contextTypes = {
+    store: PropTypes.object.isRequired
+  };
+
+  componentWillReceiveProps(nextProps) {
+    if (!this.props.user && nextProps.user) {
+      // login
+      this.props.pushState(null, '/loginSuccess');
+    } else if (this.props.user && !nextProps.user) {
+      // logout
+      this.props.pushState(null, '/');
+    }
+  }
+
+  static fetchData(getState, dispatch) {
+    const promises = [];
+    if (!isInfoLoaded(getState())) {
+      promises.push(dispatch(loadInfo()));
+    }
+    if (!isAuthLoaded(getState())) {
+      promises.push(dispatch(loadAuth()));
+    }
+    return Promise.all(promises);
+  }
+
+  handleLogout(event) {
+    event.preventDefault();
+    this.props.logout();
+  }
+
+  render() {
+    return (
+      <div>
+        <DocumentMeta {...config.app}/>
+        <div>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
